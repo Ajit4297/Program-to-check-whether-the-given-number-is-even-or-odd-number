12fb8b298893a25711f6c7eead0c9fe8125c7bc2
Ajit4297 <168507427+Ajit4297@users.noreply.github.com>
2024-04-30 18:26:35 +0530

    Add files via upload

---
+++ Untitled7.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+n=int(input())
+if n>=0:
+    if n%2==0:
+        print("even")
+    else:
+        print("odd")
+else:
+    print("invalid")
+
+
+# In[ ]:
+
+
+
+
